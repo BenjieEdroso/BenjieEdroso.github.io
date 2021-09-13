6a80cb795c21e1d7efac819609dd63d237113583
Benjie <61066771+BenjieEdroso@users.noreply.github.com>
2021-09-13 15:32:05 +0800

    add mixins

--- js/loading/loadMore.js
+++ js/loading/loadMore.js
@@ -1,15 +1,15 @@
 import { sendHttpRequest } from "../ajax/ajax.js";
 
 export function loadMore() {
-  let loadBtn = document.querySelector(".workItem__loadBtn");
+  let loadBtn = document.querySelector(".works__loadBtn");
 
   if (location == "http://127.0.0.1:5500/works.html") {
     loadBtn.addEventListener("click", () => {
       sendHttpRequest("GET", "works_template.html", loadBtn).then(
         (responseData) => {
           let limit = 8;
-          let element = document.querySelector(".workItem__grid");
-          let loadBtn = document.querySelector(".workItem__loadBtn");
+          let element = document.querySelector(".works__grid");
+          let loadBtn = document.querySelector(".works__loadBtn");
           if (element.children.length < limit) {
             element.insertAdjacentHTML("beforeend", responseData);
           }
--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ var anim1 = new Animations(
 anim1.animateWorks();
 loadMore();
 if (location == "http://127.0.0.1:5500/works.html") {
-  let work = document.querySelector(".latest__row--workItem");
+  let work = document.querySelector(".works__workItem--b1H");
   work.addEventListener("click", (e) => {
     location = "works_single.html";
   });
