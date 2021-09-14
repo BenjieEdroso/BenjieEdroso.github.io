41074993f2282222681c580e96a7c6fdf197c104
Benjie <61066771+BenjieEdroso@users.noreply.github.com>
2021-09-15 06:28:38 +0800

    revert

--- js/functions/functions.js
+++ js/functions/functions.js
@@ -8,16 +8,13 @@ let port = newURL.port;
 let pathname = newURL.pathname;
 
 function redirectToWorks() {
-<<<<<<< HEAD
   if (location == `${protocol}//${domain}:${port}/works.html`) {
-=======
-  if (location == `${protocol}//${domain}/works.html`) {
->>>>>>> parent of 04626fe (update loadmodules, functions)
     let work = document.querySelector(".works__workItem--b1H");
     work.addEventListener("click", (e) => {
-      sendHttpRequest("GET", "works_single.html").then((responseData) => {
+      sendHttpRequest("GET", "work-item/mox.html").then((responseData) => {
         let navbar = document.querySelector(".navbar");
         let works = document.querySelector(".works");
+        works.remove();
         navbar.insertAdjacentHTML("afterend", responseData);
       });
     });
